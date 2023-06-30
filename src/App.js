34e8fe26e5ec39bb72f3c554d52095dfe77e4d17
jspl151 <jayasutha151@gmail.com>
2023-06-30 17:56:29 +0530

    Images added in  the window

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import './App.scss';
 const App = () =>
 	<div className="App">
 		Ready to start the car design !
+		<div className="road"/>
 		<div className="bus">
 			<div className="window"/>
 			<div className="window1"/>
 			<div className="cover"/>
 			<div className="tire"/>
 		</div>
-		<div className="road"/>
 	</div>;
 
 export default App;
