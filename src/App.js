d1813022342d425350a76c38fbc4523b572f337a
jspl151 <jayasutha151@gmail.com>
2023-07-03 18:13:20 +0530

    Various sizes of buses.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import { React } from 'react';
 import './App.scss';
+import { rndBetween } from '@laufire/utils/lib';
+const min = 40;
+const max = 60;
+const dif = 20;
+const positionRoadTop = 58;
+const width = rndBetween(min, max);
 
-const App = () =>
-	<div className="App">
-		Ready to start the car design !
-		<div className="road"/>
-		<div className="bus">
-			<div className="window"/>
-			<div className="window1"/>
-			<div className="cover"/>
-			<div className="tire"/>
-		</div>
-	</div>;
+const App = () => <div className="App">
+	<div className="road"/>
+	<div
+		style={ {
+			width: `${ width }%`,
+			height: `${ width - dif }%`,
+			top: `${ positionRoadTop - (width - dif) }%`,
+		} }
+		className="bus"
+	>
+		<div className="window"/>
+		<div className="window1"/>
+		<div className="cover"/>
+		<div className="tire"/>
+	</div>
+</div>;
 
 export default App;
