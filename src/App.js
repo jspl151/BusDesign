96edd1ba9f070187646534c1db685fc25062d911
jspl151 <jayasutha151@gmail.com>
2023-07-03 19:14:03 +0530

    Refactored with details in config.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
 import { React } from 'react';
 import './App.scss';
-import { rndBetween } from '@laufire/utils/lib';
-const min = 40;
-const max = 60;
-const dif = 20;
-const positionRoadTop = 58;
-const width = rndBetween(min, max);
 
-const App = () => <div className="App">
-	<div className="road"/>
-	<div
-		style={ {
-			width: `${ width }%`,
-			height: `${ width - dif }%`,
-			top: `${ positionRoadTop - (width - dif) }%`,
-		} }
-		className="bus"
-	>
-		<div className="window"/>
-		<div className="window1"/>
-		<div className="cover"/>
-		<div className="tire"/>
-	</div>
-</div>;
+const App = (context) => {
+	const { config: { width, dif, roadTopPosition }} = context;
+
+	return <div className="App">
+		<div className="road"/>
+		<div
+			style={ {
+				width: `${ width }%`,
+				height: `${ width - dif }%`,
+				top: `${ roadTopPosition - (width - dif) }%`,
+			} }
+			className="bus"
+		>
+			<div className="window"/>
+			<div className="window1"/>
+			<div className="cover"/>
+			<div className="tire"/>
+		</div>
+	</div>;
+};
 
 export default App;
